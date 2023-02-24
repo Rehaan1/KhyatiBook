6c66a57d1ed4db01e2b40ef1332591361624b2ce
Rehaan1 <rehaanmazid@gmail.com>
2023-02-24 22:08:55 +0530

    feat:done

---
+++ app.js
@@ -0,0 +1,56 @@
+require('dotenv').config()
+const cors = require('cors')
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+app.use(cors())
+
+mongoose.set("strictQuery", false);
+// connect to MongoDB using mongoose
+mongoose.connect(process.env.MONGODB_DB_URI, { useNewUrlParser: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('Could not connect to MongoDB', err));
+
+// define mongoose schema
+const bookRankSchema = new mongoose.Schema({
+  bookName: { type: String, required: true },
+  bookRank: { type: Number, required: true }
+});
+
+// define mongoose model
+const BookRank = mongoose.model('BookRank', bookRankSchema);
+
+// api endpoint to get all books and their ranks
+app.get('/books', async (req, res) => {
+    const books = await BookRank.find().sort({ bookRank: 1 });
+    res.send(books);
+  });
+
+// api endpoint to post a book and its rank
+app.post('/books', async (req, res) => {
+    const { bookName, bookRank } = req.body;
+
+  // check if bookRank already exists
+  const existingBook = await BookRank.findOne({ bookRank });
+
+  if (existingBook) {
+    // increment the ranks of all books with the same rank or higher
+    await BookRank.updateMany({ bookRank: { $gte: bookRank } }, { $inc: { bookRank: 1 } });
+
+    // create a new book with the given rank
+    await BookRank.create({ bookName, bookRank });
+  } else {
+    // create a new book with the given rank
+    await BookRank.create({ bookName, bookRank });
+  }
+
+  res.send('Book added/updated successfully');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
